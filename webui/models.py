eebb0440dcecc6078162ada249f81eb11304258b
Matt Brooks <mbrooks34@gsu.edu>
2024-10-14 14:27:17 -0400

    Add description, notes, and meta to transcription model

--- webui/models.py
+++ webui/models.py
@@ -4,11 +4,14 @@ from django.db import models
 # Class: transcription
 class Transcription(models.Model):
    title = models.CharField(max_length=255)
+   description = models.TextField(null=True, default=None)
+   notes = models.TextField(null=True, default=None)
    upload_file = models.FileField(upload_to='webui/files/uploads/')
    # TODO: move upload to OR use MEDIA_ROOT & MEDIA_URL
    model = models.CharField(max_length=255)
    base_segments = models.JSONField(null=True, default=None)
    diarization = models.JSONField(null=True, default=None)
+   meta = models.JSONField(null=True, default=None)
    submitted = models.DateTimeField(auto_now=True)
 
 

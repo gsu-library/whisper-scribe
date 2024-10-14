6d50432b766e83cd9c1fd1780856cec8185a4935
Matt Brooks <mbrooks34@gsu.edu>
2024-10-14 14:58:23 -0400

    Todos

--- core/settings.sample.py
+++ core/settings.sample.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-#TODO: set defaults on added constants where used to avoid python crashing
+#TODO: put model list on this page for easy updating
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.

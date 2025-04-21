a01f568ae3863751f6699c2938e5f6375089ff62
Matt Brooks <mbrooks34@gsu.edu>
2025-04-21 09:24:44 -0400

    Cleanup code

--- webui/templatetags/time_filters.py
+++ webui/templatetags/time_filters.py
@@ -1,7 +1,5 @@
 from django import template
 
-import math
-
 
 register = template.Library()
 
@@ -16,15 +14,11 @@ def seconds_to_segment_time(value):
       minutes = total_minutes % 60
       hours = total_minutes // 60
 
-      output = f'{hours}:' if hours > 0 else ''
-
       if hours:
-         output = output + f'{minutes:02d}:'
+         output = f'{hours}:{minutes:02d}:'
       else:
          output = f'{minutes}:'
 
-      output = output + f'{seconds:02d}.{milliseconds:03d}'
-      # return f'{hours:02d}:{minutes:02d}:{seconds:02d}.{milliseconds:03d}'
-      return output
+      return output + f'{seconds:02d}.{milliseconds:03d}'
 
    return value

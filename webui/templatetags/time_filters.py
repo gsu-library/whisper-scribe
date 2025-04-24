25e8b6c2dfdb937f91bb7419f07af86d5b85ac44
Matt Brooks <mbrooks34@gsu.edu>
2025-04-24 14:32:39 -0400

    Update template tag to use format_seconds util function

--- webui/templatetags/time_filters.py
+++ webui/templatetags/time_filters.py
@@ -1,24 +1,10 @@
 from django import template
 
+from webui.utils import format_seconds
+
 
 register = template.Library()
 
 @register.filter(name='seconds_to_segment_time')
 def seconds_to_segment_time(value):
-   if isinstance(value, (int, float)):
-      total_milliseconds = int(value * 1000)
-      milliseconds = total_milliseconds % 1000
-      total_seconds = total_milliseconds // 1000
-      seconds = total_seconds % 60
-      total_minutes = total_seconds // 60
-      minutes = total_minutes % 60
-      hours = total_minutes // 60
-
-      if hours:
-         output = f'{hours}:{minutes:02d}:'
-      else:
-         output = f'{minutes}:'
-
-      return output + f'{seconds:02d}.{milliseconds:03d}'
-
-   return value
+   return format_seconds(value, segment_time=True)

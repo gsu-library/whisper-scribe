f38062c8b525b5dafb8cc73d0cfa118f93fa1c0e
Matt Brooks <mbrooks34@gsu.edu>
2025-04-24 14:31:00 -0400

    Change format_timestamp to format_seconds and add segment_time parameter

--- webui/utils.py
+++ webui/utils.py
@@ -14,23 +14,30 @@ def is_float(number):
       return False
 
 
-# Function: format_timestamp
-def format_timestamp(seconds: float, always_include_hours: bool = False, decimal_marker: str = '.', include_mill = True):
-   assert seconds >= 0, 'non-negative timestamp expected'
-   milliseconds = round(seconds * 1000.0)
+# Function: format_seconds
+def format_seconds(seconds, always_include_hours = False, decimal_marker = '.', include_mill = True, segment_time = False):
+   if not isinstance(seconds, (int, float)):
+      return seconds
 
+   milliseconds = round(seconds * 1000.0)
    hours = milliseconds // 3_600_000
    milliseconds -= hours * 3_600_000
-
    minutes = milliseconds // 60_000
    milliseconds -= minutes * 60_000
-
    seconds = milliseconds // 1_000
    milliseconds -= seconds * 1_000
 
-   hours_marker = f'{hours:02d}:' if always_include_hours or hours > 0 else ''
-   mills = f'{decimal_marker}{milliseconds:03d}' if include_mill else ''
-   return (f'{hours_marker}{minutes:02d}:{seconds:02d}{mills}')
+   if segment_time:
+      if hours:
+         total = f'{hours:d}:{minutes:02d}:{seconds:02d}.{milliseconds:03d}'
+      else:
+         total = f'{minutes:d}:{seconds:02d}.{milliseconds:03d}'
+   else:
+      hours_marker = f'{hours:02d}:' if always_include_hours or hours > 0 else ''
+      mills = f'{decimal_marker}{milliseconds:03d}' if include_mill else ''
+      total = f'{hours_marker}{minutes:02d}:{seconds:02d}{mills}'
+
+   return total
 
 
 # Function: get_file_duration

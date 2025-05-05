d0777425811966ef09e1a3b7a9a2dab77726ce4e
Matt Brooks <mbrooks34@gsu.edu>
2025-05-05 09:37:15 -0400

    Add a function to get the README version

--- webui/utils.py
+++ webui/utils.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.core.cache import cache
 
 from pathlib import Path
 import subprocess
@@ -12,6 +13,29 @@ def is_float(number):
       return True
    except ValueError:
       return False
+
+
+# Function: get_version
+def get_version(prefix = ''):
+   README = 'README.md'
+   CACHE_NAME = 'readme_version'
+   CACHE_TIMEOUT = 300
+   version = cache.get(CACHE_NAME)
+
+   if version:
+      return prefix + version
+
+   try:
+      with open(settings.BASE_DIR / README, 'r') as f:
+         for line in f:
+            if line.startswith('Version:'):
+               version = line[len('Version:'):].strip()
+               cache.set(CACHE_NAME, version, CACHE_TIMEOUT)
+               return prefix + version
+   except FileNotFoundError:
+      print(f'README file not found.')
+
+   return prefix
 
 
 # Function: format_seconds

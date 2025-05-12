4103899864b6a6709e7160744922cfac6dd143f6
Matt Brooks <mbrooks34@gsu.edu>
2025-05-12 15:03:03 -0400

    Start working on transcription table

--- webui/static/webui/js/list.js
+++ webui/static/webui/js/list.js
@@ -1,5 +1,32 @@
 (() => {
    'use strict';
 
-   let transcriptionTable = new DataTable('#transcriptions');
+
+   // Check before deleting transcription
+   const deleteButtons = document.querySelectorAll('.delete-transcription');
+   deleteButtons.forEach(button => {
+      button.addEventListener('click', event => {
+         const titleLink = event.target.closest('tr').querySelector('.title a');
+         const confirmation = confirm('Are you sure you want to delete the transcription "' + titleLink.textContent + '"?');
+
+         if(!confirmation) {
+            event.stopPropagation();
+            event.preventDefault();
+         }
+      });
+   });
+
+
+   // Initialize and configure DataTables
+   let transcriptionsTable = new DataTable('#transcriptions', {
+      columns: [
+         null,
+         null,
+         { searchable: false, orderable: false },
+         { searchable: false, orderable: false },
+         { searchable: false, orderable: false },
+         { searchable: false, orderable: false },
+         { searchable: false, orderable: false }
+      ]
+   });
 })();

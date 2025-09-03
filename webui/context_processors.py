6cba5c26f1945725c9a3b446dab344294abfcd22
Matt Brooks <mbrooks34@gsu.edu>
2025-09-03 15:11:32 -0400

    Add notes

--- webui/context_processors.py
+++ webui/context_processors.py
@@ -1,7 +1,10 @@
 from .utils import get_version
 
 
-# Function webui
 def webui(request):
+   """
+   Create a context processor for the project. Provides the current version of the
+   application.
+   """
    version = get_version()
    return { 'version': version }
